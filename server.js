e7891011b287f540febaa200e087d0e5e7bd7cf9
Francisco Marques <franciscomarques31@gmail.com>
2025-12-29 17:24:54 +0000

    Atualizações backend

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
+require("dotenv").config(); // ⬅️ PRIMEIRA LINHA
+
 const express = require("express");
-require("dotenv").config();
 const connectDB = require("./config/db");
 const cors = require("cors");
 const favoritesRoutes = require("./routes/favoritesRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://agromarket-frontend-eight.vercel.app']
+  origin: [
+    "http://localhost:5173",
+    "https://agromarket-frontend-eight.vercel.app"
+  ]
 }));
 
 connectDB();
-
 
 app.use(express.json());
 
@@ -25,12 +27,12 @@ app.use("/api/users/favorites", favoritesRoutes);
 app.use("/api/messages", require("./routes/messageRoutes"));
 app.use("/api/admin", adminRoutes);
 
-
 app.get("/", (req, res) => res.send("🚀 Servidor a correr com MongoDB!"));
 
-
-if (require.main == module){
-  app.listen(PORT, () => console.log(`Servidor a correr na porta ${PORT}`));
+if (require.main === module) {
+  app.listen(PORT, () =>
+    console.log(`Servidor a correr na porta ${PORT}`)
+  );
 }
 
 module.exports = app;
